44a69f2f17418beef4e1d4337ebd9a18bf19d343
Minh Nguyen <xiuquay55@gmail.com>
2022-04-27 12:42:10 +0700

    add mutation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 'use strict';
 
-const { GraphQLSchema, GraphQLObjectType, GraphQLList } = require('winext-repo-store').graphql;
+const { GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLNonNull, GraphQLString } =
+  require('winext-repo-store').graphql;
 
 const AuthorModel = require('./models/author-model');
 const PostModel = require('./models/post-model');
 
+/**
+ * Query
+ */
 const queryRootSchema = new GraphQLObjectType({
   name: 'QueryRootAppSchema',
-  description: 'Application Schema Root',
+  description: 'Application Query Schema Root',
   fields: () => ({
     authors: {
       type: new GraphQLList(AuthorModel),
@@ -25,9 +29,33 @@ const queryRootSchema = new GraphQLObjectType({
     },
   }),
 });
+/**
+ * Mutation
+ */
+const mutationRootSchema = new GraphQLObjectType({
+  name: 'MutationRootAppSchema',
+  description: 'Application Mutation Schema Root',
+  fields: () => ({
+    author: {
+      type: AuthorModel,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve: async (parent, args, context, resolveInfo) => {
+        try {
+          console.log('🚀 ~ file: index.js ~ line 45 ~ resolve: ~ args', args);
+          console.log('🚀 ~ file: index.js ~ line 45 ~ resolve: ~ parent', parent);
+        } catch (err) {
+          throw new Error('Failed to insert new author');
+        }
+      },
+    },
+  }),
+});
 
 const schemas = new GraphQLSchema({
   query: queryRootSchema,
+  mutation: mutationRootSchema,
 });
 
 module.exports = schemas;
